63bec74e627a2fdc868d4959c054a22d7222e73f
fesgic <festusgichohi2@gmail.com>
2022-03-10 16:50:20 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 /**
- * C program
- * Prints out using puts
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	/**
-	 * Use puts
-	*/
-
-	char message[] = "Programming is like building a multilingual puzzle\n";
+	char message[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(message);
 	return (0);

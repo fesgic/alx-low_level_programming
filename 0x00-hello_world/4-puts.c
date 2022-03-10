3351d7020910c88461d83a178c98a5a8e12eebe9
fesgic <festusgichohi2@gmail.com>
2022-03-10 16:13:05 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * C program
+ * Prints out using puts
+*/
+
+int main(void)
+{
+	/**
+	 * Use puts
+	*/
+
+	char message[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(message);
+	return (0);
+}

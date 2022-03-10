3925d5c973abf1b47923dd7971777dd621393a58
fesgic <festusgichohi2@gmail.com>
2022-03-10 18:42:26 +0300

    display to stdo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int arrSize;
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	arrSize = sizeof(msg) / sizeof(msg[1]);
 	write(1, msg, arrSize);

a9e92a262fb01fbc6242d8dd85bf7dcbd3475360
fesgic <festusgichohi2@gmail.com>
2022-03-11 23:27:21 +0300

    base 10 no's using puts

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success(0)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}

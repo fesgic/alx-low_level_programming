117355dfe11da52d0904961e75e0719323d9fa08
fesgic <festusgichohi2@gmail.com>
2022-03-16 01:08:11 +0300

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,13 @@ int print_last_digit(int n)
 {
 	int a;
 
-	if (n >= 0)
+	if (n < 0)
 	{
-		_putchar(n % 10 + '0');
-		a = (n % 10);
+		n = -n;
 	}
-	else if (n < 0)
-	{
-		_putchar(-n % 10 + '0');
-		a = (-n % 10);
-	}
+
+	a = n % 10;
+	_putchar(a + '0');
 	return (a);
+
 }

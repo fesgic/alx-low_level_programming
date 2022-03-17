e92a3b3f84b047a731d4b4019d128d33bc49752b
fesgic <festusgichohi2@gmail.com>
2022-03-18 00:20:27 +0300

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		a++;
+		_putchar('\n');
+	}
+}

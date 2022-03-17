d6f4fc16a26df0af9f0d14fabc53df180a9aab66
fesgic <festusgichohi2@gmail.com>
2022-03-17 19:03:49 +0300

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints line depending on value passed
+ * @n: value of line to print
+ *
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}

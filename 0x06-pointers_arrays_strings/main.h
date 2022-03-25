bb7482cde78f69c950487d587dbc7a7c96ed6e20
fesgic <festusgichohi2@gmail.com>
2022-03-25 04:28:11 +0300

    print buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,5 @@ int _strcmp(char *s1, char *s2);
 char *rot13(char *a);
 void print_number(int n);
 char *_strncpy(char *dest, char *src, int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);

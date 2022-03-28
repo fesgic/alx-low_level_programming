ce1269db49f14f6381b12714f6792328e738bcc9
fesgic <festusgichohi2@gmail.com>
2022-03-28 17:15:24 +0300

    string locate character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a char in string
+ * @s: string provided
+ * @c: character to locate
+ *
+ * Return: return pointer to first occurence
+ */
+char *_strchr(char *s, char c)
+{
+
+}

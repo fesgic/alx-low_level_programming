d627eb80b1ecf865aa112591207c270b2329e346
fesgic <festusgichohi2@gmail.com>
2022-03-29 02:05:49 +0300

    DOuble pointer, double fan

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
+void set_string(char **s, char *to);

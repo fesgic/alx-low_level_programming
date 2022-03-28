ccf369b7adcd97bb1bf1afd0bb36a69e0d951c0d
fesgic <festusgichohi2@gmail.com>
2022-03-29 00:55:47 +0300

    print chess board

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,4 @@ void print_diagsums(int *a, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
-int _putchar(char c)
+int _putchar(char c);

78a38f84d84505b91162c05be52975c811e0f5e0
fesgic <festusgichohi2@gmail.com>
2022-03-30 16:10:48 +0300

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - returns facotorial of no
+ * @n: number to get facotorial
+ *
+ * Return: factorial of no
+ *
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}

60131efd9e4567254593a8bb1e1faadb3b3555f1
fesgic <festusgichohi2@gmail.com>
2022-04-01 19:44:10 +0300

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: no of args
+ * @argv: arguments
+ *
+ * Return: success(1)
+ *
+ */
+int main(int argc, char **argv)
+{
+	if (*argv)
+	{
+		printf("%d\n", argc);
+	}
+
+	return (0);
+}

70da4c5728354c257bd0c4227463510107ebbc25
fesgic <festusgichohi2@gmail.com>
2022-04-13 23:28:12 +0300

    print name passed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print passed name
+ * @name: passed named
+ * @f: function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}

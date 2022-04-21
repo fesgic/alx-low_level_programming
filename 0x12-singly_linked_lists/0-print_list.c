7904983d0b408f1eecf848aeea1ac76dc1ab8b5a
fesgic <festusgichohi2@gmail.com>
2022-04-22 02:59:46 +0300

    print a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all elements
+ * of a list_t list
+ * @h: elements of list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 1;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	if (h->next == NULL)
+		return (1);
+	if (h->next != NULL)
+	{
+		h = h->next;
+		count += print_list(h);
+	}
+	return (count);
+
+}

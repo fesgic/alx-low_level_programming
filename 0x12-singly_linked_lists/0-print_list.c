cfa8c2a34163036a0ac22302126cf843939eda76
fesgic <festusgichohi2@gmail.com>
2022-04-23 02:48:11 +0300

    prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,21 @@ size_t print_list(const list_t *h)
 {
 	unsigned int count = 1;
 
+	if (h == NULL)
+		EXIT_FAILURE;
+
 	if (h->str == NULL)
 	{
 		printf("[0] (nil)\n");
 	}
 	printf("[%d] %s\n", h->len, h->str);
-	if (h->next == NULL)
-		return (1);
 	if (h->next != NULL)
 	{
 		h = h->next;
 		count += print_list(h);
 	}
+	else if (h->next == NULL)
+		return (1);
 	return (count);
 
 }

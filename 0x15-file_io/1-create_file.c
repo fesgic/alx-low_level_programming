347efa2e25d3d84f70c6bccd3dc2a5d6cecfc721
fesgic <festusgichohi2@gmail.com>
2022-05-03 21:49:04 +0300

    write files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	result = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (result < 0)

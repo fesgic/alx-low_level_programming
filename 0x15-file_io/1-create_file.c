f7c2ef19ef73d5e269c099572c25af5b0fbab2d3
fesgic <festusgichohi2@gmail.com>
2022-05-03 19:45:40 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,22 @@
 #include "main.h"
 
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/**
+ * free_space - free memory
+ * @fd: file descriptor
+ * @space: memory
+ *
+ */
+void free_space(int fd, int *space)
+{
+	free(space);
+	close(fd);
+}
 /**
  * create_file - create a file
  * @filename: file to create
@@ -43,8 +55,14 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	write(fd, text_content, count);
-	close(fd);
-	free(text);
+	if (text_content == NULL)
+	{
+		free_space(fd, text);
+	}
+	else
+	{
+		write(fd, text_content, count);
+		free_space(fd, text);
+	}
 	return (0);
 }

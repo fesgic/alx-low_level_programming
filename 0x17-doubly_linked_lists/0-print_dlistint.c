a7b6ecaa0a3710314d930c5aa66963c9786c7962
fesgic <festusgichohi2@gmail.com>
2022-06-09 16:25:05 +0300

    print a double list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print list
+ * @h: list
+ *
+ * Return: no of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *head = malloc(sizeof(dlistint_t));
+
+	if (head == NULL)
+		EXIT_FAILURE;
+	head = (h->next)->prev;
+	while (head->prev != NULL)
+		head = head->prev;
+	while (head->next != NULL)
+	{
+		printf("%d\n", head->n);
+		count++;
+		head = head->next;
+	}
+	printf("%d\n", head->n);
+	count++;
+	return (count);
+}
